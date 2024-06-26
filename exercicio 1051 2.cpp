867f8abaa567383ddc29cfa305482b45a7b12146
AmandaSouza07 <amand4desouzaa@gmail.com>
2024-06-26 11:03:19 -0300

    Add files via upload

---
+++ exercicio 1051 2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+	float salario;
+	cin>>salario;
+	
+	cout<< salario * 20 << endl;
+	/*(salario % 2000)%1000
+	((salario % 2000)%1000)
+*/
+
+return 0;
+}
